c2e02fb5d300a6e08ed53fd93ef6e05a1411ff64
毒牙 <34503154+J-DuYa@users.noreply.github.com>
2021-05-20 21:31:31 +0800

    feat: 1. 主函数

---
+++ src/utils/babel.js
@@ -0,0 +1,11 @@
+import * as BabelStandOne from '@babel/standalone';
+import * as BabelTypes from '@babel/types';
+import * as BabelParser from '@babel/parser';
+import * as BabelGenerator from '@babel/generator';
+
+export {
+	BabelStandOne,
+	BabelTypes,
+	BabelParser,
+	BabelGenerator
+};
